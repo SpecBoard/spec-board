3b48221c6fd1ac0f54bc3be5abcec770c14f7e5f
Mihucza, Bence <mihben@gmail.com>
2025-03-29 16:47:56 +0100

    test: Implemented unit test for project overview

--- src/client/src/modules/project/__test_utils__/project-faker.ts
+++ src/client/src/modules/project/__test_utils__/project-faker.ts
@@ -5,6 +5,56 @@ export class ProjectFaker {
   public static random(): Project {
     return {
       key: `${faker.string.alpha()}_${faker.string.alpha()}`,
+      version: faker.system.semver(),
+      lastReport: faker.date.recent(),
+      passCount: faker.number.int(),
+      failCount: faker.number.int(),
+      skippedCount: faker.number.int(),
     };
+  }
+
+  public static pass(): Project {
+    const result = this.random();
+    result.failCount = 0;
+    result.skippedCount = 0;
+
+    return result;
+  }
+
+  public static fail(): Project {
+    const result = this.random();
+    result.passCount = 0;
+    result.skippedCount = 0;
+
+    return result;
+  }
+
+  public static skipped(): Project {
+    const result = this.random();
+    result.passCount = 0;
+    result.failCount = 0;
+
+    return result;
+  }
+
+  public static notPass(): Project {
+    const result = this.random();
+    result.passCount = 0;
+
+    return result;
+  }
+
+  public static notFail(): Project {
+    const result = this.random();
+    result.failCount = 0;
+
+    return result;
+  }
+
+  public static notSkipped(): Project {
+    const result = this.random();
+    result.skippedCount = 0;
+
+    return result;
   }
 }
--- src/client/src/modules/project/components/overview/overview.component.spec.ts
+++ src/client/src/modules/project/components/overview/overview.component.spec.ts
@@ -21,9 +21,97 @@ describe('OverviewComponent', () => {
 
     // Assert
     cut.detectChanges();
-    expect(cut.query(byTestId('divAvatar'))?.textContent).toEqual(
-      getAvatar(project.key)
-    );
+    expect(cut.query(byTestId('divAvatar'))?.textContent).toEqual(getAvatar(project.key));
+  });
+
+  it('[UNIT][POC-002]: Show Status as Pass', () => {
+    // Arrange
+    const project = ProjectFaker.pass();
+
+    // Act
+    const cut = createCUT({
+      props: {
+        project: project,
+      },
+    });
+
+    // Assert
+    expect(cut.query(byTestId('secOverview'))?.classList).toContain('pass');
+  });
+
+  it('[UNIT][POC-003]: Show Status as Fail', () => {
+    // Arrange
+    const project = ProjectFaker.fail();
+
+    // Act
+    const cut = createCUT({
+      props: {
+        project: project,
+      },
+    });
+
+    // Assert
+    expect(cut.query(byTestId('secOverview'))?.classList).toContain('fail');
+  });
+
+  it('[UNIT][POC-004]: Show Status as Skipped', () => {
+    // Arrange
+    const project = ProjectFaker.skipped();
+
+    // Act
+    const cut = createCUT({
+      props: {
+        project: project,
+      },
+    });
+
+    // Assert
+    expect(cut.query(byTestId('secOverview'))?.classList).toContain('skipped');
+  });
+
+  it('[UNIT][POC-005]: Show Pass is Not Relevant', () => {
+    // Arrange
+    const project = ProjectFaker.notPass();
+
+    // Act
+    const cut = createCUT({
+      props: {
+        project: project,
+      },
+    });
+
+    // Assert
+    expect(cut.query(byTestId('pdgPass'))?.classList).toContain('not-relevant');
+  });
+
+  it('[UNIT][POC-006]: Show Fail is Not Relevant', () => {
+    // Arrange
+    const project = ProjectFaker.notFail();
+
+    // Act
+    const cut = createCUT({
+      props: {
+        project: project,
+      },
+    });
+
+    // Assert
+    expect(cut.query(byTestId('pdgFail'))?.classList).toContain('not-relevant');
+  });
+
+  it('[UNIT][POC-006]: Show Fail is Not Relevant', () => {
+    // Arrange
+    const project = ProjectFaker.notSkipped();
+
+    // Act
+    const cut = createCUT({
+      props: {
+        project: project,
+      },
+    });
+
+    // Assert
+    expect(cut.query(byTestId('pdgSkipped'))?.classList).toContain('not-relevant');
   });
 
   const getAvatar = (key: string): string => {
