339706d1447387ed05063effb79d8c6d4b8960fa
Mihucza, Bence <mihben@gmail.com>
2025-04-02 12:15:44 +0200

    Exclude logger from coverage

--- src/client/jest.config.ts
+++ src/client/jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup.jest.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/libs/'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/modules/logger/'],
 };
 
 export default config;
