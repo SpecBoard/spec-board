9fbee2ead87c30586fb468f36427fd2fb451933b
Mihucza, Bence <mihben@gmail.com>
2025-06-07 00:38:54 +0200

    chore(Client): Added configuration

--- src/SpecBoard.Client.Angular/src/app/app.config.ts
+++ src/SpecBoard.Client.Angular/src/app/app.config.ts
@@ -1,8 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import {
+  defaultConfiguration,
+  provideConfiguration,
+} from '@mihben/ngx-configuration';
+
 import { routes } from './app.routes';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    provideHttpClient(withInterceptorsFromDi()),
+
+    provideConfiguration((builder) =>
+      defaultConfiguration(builder, 'Development')
+    ),
+  ],
 };
