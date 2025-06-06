e45beffa92f76c5c4fbb897dadb740287a6aca57
Mihucza, Bence <mihben@gmail.com>
2025-06-06 23:20:18 +0200

    chore(BFF): Integrated S-Train

--- src/SpecBoard.Web/Program.cs
+++ src/SpecBoard.Web/Program.cs
@@ -1,12 +1,17 @@
 using Serilog;
+using SpecBoard.Web.Wireup;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Host.UseLightInject();
 builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
+
+builder.AddDependencies();
+builder.AddCQS();
+builder.AddDependencies();
 
 var app = builder.Build();
 
---
+++ src/SpecBoard.Web/Wireup/Wireups.cs
@@ -0,0 +1,36 @@
+﻿using STrain.CQS.NetCore;
+using STrain.CQS.NetCore.Builders;
+using STrain.CQS.NetCore.LigtInject;
+
+namespace SpecBoard.Web.Wireup
+{
+	public static class Wireups
+	{
+		public static void AddDependencies(this WebApplicationBuilder builder)
+		{
+		}
+
+		public static void AddCQS(this WebApplicationBuilder builder)
+		{
+			builder.AddCQS(builder =>
+			{
+				builder.AddRequestValidator()
+					.UseFluentRequestValidator(builder => { });
+
+				builder.AddRequestRouter(request =>
+				{
+					if (request.GetType().Namespace.StartsWith("SpecProfile")) return "specprofile";
+					else return "readmodel";
+				}, builder =>
+				{
+					builder.AddGenericHttpSender("specprofile", (options, confiugration) => confiugration.Bind("SpecProfile", options));
+					builder.AddGenericHttpSender("readmodel", (options, configuration) => configuration.Bind("ReadModel", options));
+				});
+			});
+		}
+
+		public static void AddEventing(this WebApplicationBuilder builder)
+		{
+		}
+	}
+}
