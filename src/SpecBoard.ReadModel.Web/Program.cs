ae18647741b359203afba4b87de17011f1a72296
Mihucza, Bence <mihben@gmail.com>
2025-06-06 22:46:05 +0200

    chore(ReadModel): Integrated Serilog

--- src/SpecBoard.ReadModel.Web/Program.cs
+++ src/SpecBoard.ReadModel.Web/Program.cs
@@ -1,8 +1,14 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
 builder.Services.AddControllers();
+
+
 
 var app = builder.Build();
 
